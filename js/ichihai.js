fd08327343d04b6318a6f0195c6b9c97797d2eb6
Ichihai1415 <projects3141.gif@gmail.com>
2024-05-25 21:53:59 +0900

    cookieをちょっと

---
+++ js/ichihai.js
@@ -0,0 +1,34 @@
+/**
+ *Cookieの配列を取得します。
+ * @returns {Array} Cookieの配列
+ */
+function getCookies() {
+    //https://qiita.com/mocha_xx/items/e0897e9f251da042af59
+    /**
+     * @type {Array} Cookieの配列
+     */
+    let cookieArr = new Array();
+    const cookie = decodeURIComponent(document.cookie);
+    if (cookie != "") {
+        console.log("[getCookies]cookie(raw): " + cookie);
+        const cookies = document.cookie.split("; ");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie_ = cookies[i].split("=");
+            cookieArr[cookie_[0]] = cookie_[1];
+        }
+    } else {
+        console.error("cookie not found.");
+    }
+    return cookieArr;
+}
+
+/**
+ * Cookieを保存します。
+ * @param {*} value 保存する値("name=data"など形式)
+ * @param {*} maxAge 有効期間(秒) 既定は30日
+ */
+function setCookie(value, maxAge = 2592000) {
+    const saveCookie = value + "; max-age=" + maxAge + "; ";
+    document.cookie = saveCookie;
+    console.log("[setCookie]cookie saved: " + saveCookie);
+}
