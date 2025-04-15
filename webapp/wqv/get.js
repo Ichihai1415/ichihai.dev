88af62d1cdf6f3111eeb39b75403f4d3747e3098
Ichihai1415 <projects31415.gif@gmail.com>
2025-04-15 12:06:40 +0900

    WQVfW仮追加等

--- webapp/wqv/get.js
+++ webapp/wqv/get.js
@@ -22,7 +22,24 @@ function getU() {
         "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
     getRawData(url)
         .then((data) => {
-            console.log(data);
+            const json = JSON.parse(data);
+            console.log(json);
+            let newTable =
+                "<tr><th>発生日時</th><th>震央</th><th>マグニチュード</th><th>深さ</th></tr>";
+            json.features.forEach((element) => {
+                //console.log(element);
+                const time = new Date(element.properties.time).toLocaleString(
+                    "ja-JP",
+                    {
+                        timeZone: "Asia/Tokyo",
+                    }
+                );
+                const hypoName = element.properties.place;
+                const magnitude = element.properties.mag;
+                const depth = element.geometry.coordinates[2];
+                newTable += `<tr><td>${time}</td><td>${hypoName}</td><td>${magnitude}</td><td>${depth}km</td></tr>`;
+            });
+            document.getElementById("infoT").innerHTML = newTable;
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
@@ -34,21 +51,7 @@ function getG() {
         "https://geofon.gfz-potsdam.de/fdsnws/event/1/query?format=text&minmag=4.5&limit=10&end=2100-01-01";
     getRawData(url)
         .then((data) => {
-            console.log(data);
-        })
-        .catch((error) => {
-            console.error("Error fetching data:", error);
-        });
-}
-
-
-
-function getJ() {
-    const url =
-        "https://www.data.jma.go.jp/developer/xml/feed/extra.xml";
-    getRawData(url)
-        .then((data) => {
-            console.log(data);
+            document.getElementById("infoT").innerHTML = data;
         })
         .catch((error) => {
             console.error("Error fetching data:", error);
---
+++ webapp/wqv/other.js
@@ -0,0 +1,4 @@
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = (window.innerWidth * 9) / 16;
+}
