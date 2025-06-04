8018208bdb6029ff129870435a551651eab276e5
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-04 22:19:23 +0900

    デザイン調整

--- js/common.js
+++ js/common.js
@@ -2,7 +2,24 @@
 
 fetch("/parts/header.html")
     .then((response) => response.text())
-    .then((data) => (document.querySelector("header").innerHTML = data));
+    .then((data) => (document.querySelector("header").innerHTML = data))
+    .then(() => {//ここに入れないと動かない
+        document
+            .querySelector(".header-menu-icon-wrap")
+            .addEventListener("click", function () {
+                const headerSub = document.querySelector(".header-sub");
+                if (headerSub.style.display == "none") {
+                    headerSub.style.display = "flex";
+                    document.querySelector(".header-menu-icon").src =
+                        "/parts/sansen_sub.svg";
+                } else {
+                    headerSub.style.display = "none";
+                    document.querySelector(".header-menu-icon").src =
+                        "/parts/sansen.svg";
+                }
+                console.log(headerSub.style.display);
+            });
+    });
 
 fetch("/parts/footer.html")
     .then((response) => response.text())
