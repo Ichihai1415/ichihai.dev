f98c6a20df1e96c4dfa5d78d541b580a15ca22fc
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-25 17:14:05 +0900

    2~7の修正　暗号化してなくても表示　404ルーティング変更テスト

---
+++ functions/_middleware.ts
@@ -0,0 +1,16 @@
+// functions/_middleware.ts
+export const onRequest = async (context) => {
+    const url = new URL(context.request.url);
+    const isHtml = url.pathname.endsWith(".html");
+
+    const asset = await context.env.ASSETS.fetch(context.request);
+
+    if (asset.status === 404 && isHtml) {
+        const notFoundPage = await context.env.ASSETS.fetch(
+            "https://ichihai.dev/404.html"
+        );
+        return new Response(await notFoundPage.text(), { status: 404 });
+    }
+
+    return asset;
+};
