d73becaf60ea99a2ca94d220b468c1d67d4daed3
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-25 17:32:11 +0900

    404ルート調整

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,16 +1,11 @@
-// functions/_middleware.ts
 export const onRequest = async (context) => {
     const url = new URL(context.request.url);
-    console.log(context.request.url);
-    const isHtml = url.pathname.endsWith(".html");
-
+    const isDat = url.pathname.endsWith(".dat");
     const asset = await context.env.ASSETS.fetch(context.request);
 
-    if (asset.status === 404 && isHtml) {
-        const notFoundPage = await context.env.ASSETS.fetch(
-            "https://ichihai.dev/404.html"
-        );
-        return new Response(await notFoundPage.text(), { status: 404 });
+    if (asset.status === 404 && isDat) {
+        return asset;
     }
-    return asset;
+
+    return new Response(null, { status: 404 });
 };
