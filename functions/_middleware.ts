5cbeaa9ac86716c2e6bece4d3c632964c1d7cbad
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-25 17:24:31 +0900

    consoleテスト2

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,6 +1,7 @@
 // functions/_middleware.ts
 export const onRequest = async (context) => {
     const url = new URL(context.request.url);
+    console.log(context.request.url);
     const isHtml = url.pathname.endsWith(".html");
 
     const asset = await context.env.ASSETS.fetch(context.request);
@@ -9,9 +10,7 @@ export const onRequest = async (context) => {
         const notFoundPage = await context.env.ASSETS.fetch(
             "https://ichihai.dev/404.html"
         );
-        console.log("404 page");
         return new Response(await notFoundPage.text(), { status: 404 });
     }
-    console.log("404 response");
     return asset;
 };
