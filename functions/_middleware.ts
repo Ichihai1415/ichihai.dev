f6176fdb960278733643140519af4e71fedce598
Ichihai1415 <projects3141.gif@gmail.com>
2025-06-25 17:18:56 +0900

    function consoleテスト

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -9,8 +9,9 @@ export const onRequest = async (context) => {
         const notFoundPage = await context.env.ASSETS.fetch(
             "https://ichihai.dev/404.html"
         );
+        console.log("404 page");
         return new Response(await notFoundPage.text(), { status: 404 });
     }
-
+    console.log("404 response");
     return asset;
 };
